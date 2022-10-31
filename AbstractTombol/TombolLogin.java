f1d274a3cbc429d66231e1030c0c70ac55c1cfc8
febriandfa <114402867+febriandfa@users.noreply.github.com>
2022-11-01 05:40:02 +0700

    Add files via upload

---
+++ AbstractTombol/TombolLogin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.abstrakinterfes.abstrak;
+
+/**
+ *
+ * @author Asus
+ */
+class TombolLogin extends Tombol {
+    @Override
+    public void tekan(){
+        System.out.println("Berhasil Login!");
+    }
+}
---
+++ AbstractTombol/TombolNuklir.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.abstrakinterfes.abstrak;
+
+/**
+ *
+ * @author Asus
+ */
+class TombolNuklir extends Tombol {
+    @Override
+    public void tekan() {
+        System.out.println("Bom Nuklir Telah Diluncurkan!");
+        System.out.println("Booooom!!!");
+    }
+}
